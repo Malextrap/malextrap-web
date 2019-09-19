ea47132400abd1a660e63585126d3018b20bb127
Alejandro Rios <contact@clonalejandro.me>
2019-09-19 12:34:52 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Home from './pages/home';
-import Components from "./pages/componenst";
+import Components from "./pages/components";
 import Documentation from "./pages/documentation";
 
 export default () =>
